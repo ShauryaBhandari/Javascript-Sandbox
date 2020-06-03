16b7defc976529acb5994ec1918ccb79554d8466
Shaurya Bhandari <shauryabhandari25@gmail.com>
2020-06-03 23:32:04 +0530

    Updated

---
+++ JS Sandbox/21. New features ES 2015+/1. Iterators and generators/app.js
@@ -0,0 +1,55 @@
+// Iterator Example
+// function nameIterator(names) {
+//   let nextIndex = 0;
+
+//   return {
+//     next: function() {
+//       return nextIndex < names.length ?
+//       { value: names[nextIndex++], done: false } :
+//       { done: true }
+//     }
+//   }
+// }
+
+// // Create an array of names
+// const namesArr = ['Jack', 'Jill', 'John'];
+// // Init iterator and pass in the names array
+// const names = nameIterator(namesArr);
+
+// console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next().value);
+
+// Generator are functions than can return multiple values add * after function to make ig a generator
+
+// function* sayNames() {
+//   yield 'Jack';
+//   yield 'Jill';
+//   yield 'John';
+// }
+
+// const name = sayNames();
+
+// console.log(name.next().value);
+// console.log(name.next().value);
+// console.log(name.next().value);
+// console.log(name.next().value);
+
+// ID Creator
+function* createIds() {
+  let index = 1;
+
+  while (true) {
+    yield index++;
+  }
+}
+
+const gen = createIds();
+
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
